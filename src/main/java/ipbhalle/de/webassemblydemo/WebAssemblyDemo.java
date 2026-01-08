ee79de157492c789633e1c2e48ed19f4c000ee71
FabianMauz <fmauz@ipb-halle.de>
2026-01-08 13:39:59 +0100

    op: add runscript

--- src/main/java/ipbhalle/de/webassemblydemo/WebAssemblyDemo.java
+++ src/main/java/ipbhalle/de/webassemblydemo/WebAssemblyDemo.java
@@ -3,13 +3,13 @@ package ipbhalle.de.webassemblydemo;
 import org.teavm.interop.Export;
 
 public class WebAssemblyDemo {
-       
+
     public static void main(String[] args) {
         System.out.println("TeaVM/WebAssembly Demo classpath is successfully loaded.");
     }
 
     @Export(name = "addNumbers")
-    public static int add(int a, int b) {       
-        return a + b;
+    public static int add(int a, int b) {
+        return a + b * 2;
     }
 }
