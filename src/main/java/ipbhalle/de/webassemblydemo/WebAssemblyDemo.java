c4a8eca4d65b705091fb7b9afd1b210efb7c855a
Fabian Mauz <fmauz@ipb-halle.de>
2026-01-08 13:28:43 +0100

    init: initial commit

---
+++ src/main/java/ipbhalle/de/webassemblydemo/WebAssemblyDemo.java
@@ -0,0 +1,15 @@
+package ipbhalle.de.webassemblydemo;
+
+import org.teavm.interop.Export;
+
+public class WebAssemblyDemo {
+       
+    public static void main(String[] args) {
+        System.out.println("TeaVM/WebAssembly Demo classpath is successfully loaded.");
+    }
+
+    @Export(name = "addNumbers")
+    public static int add(int a, int b) {       
+        return a + b;
+    }
+}
